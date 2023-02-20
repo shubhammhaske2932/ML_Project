5362b53f45c8503374efc863da9d3254c4901c61
shubhammhaske2932 <aiml.learning7@gmail.com>
2023-02-20 23:44:35 +0530

    Created Dataingestion.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.3"
+VERSION="0.0.1"
 AUTHOR="Shubham Mhaske"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
